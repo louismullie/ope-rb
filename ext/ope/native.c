9a6964c2109e4331a908db7f41f6b54ca8e737f7
Louis Mullie <louis.mullie@gmail.com>
2014-04-15 15:46:08 -0400

    Clean up beginning of C impl

--- ext/ope/native.c
+++ ext/ope/native.c
@@ -46,62 +46,7 @@ static int imin2(int x, int y) {
 }
 
 static VALUE ope_rb;
-static VALUE ope_rb_cipher;
-
-/* 
- * Initialize an OPE::Cipher object with a key.
- */
-static VALUE ope_rb_initialize(VALUE self, VALUE key) {
- 
-  int keyLen;
-  
-  // Replace key value with key.to_str
-  StringValue(key);
-  
-  // Get the key length as an int.
-  keyLen = RSTRING_LEN(key);
-  
-  // Make sure key is not empty
-  if (keyLen == 0) {
-    rb_raise(rb_eArgError, "Key must be non-empty.");
-  }
-  
-  // Set key as instance variable
-  rb_iv_set(self, "@key", key);
-  
-  return self;
-  
-}
-
-static unsigned char* ope_rb_get_key(VALUE self) {
-  
-  VALUE rbKey;
-  unsigned char* cKey;
-  
-  // Get the key instance variable
-  rbKey = rb_iv_get(self, "@key");
-  StringValue(rbKey);
-  
-  // Convert the key to a byte array
-  cKey = (unsigned char*) RSTRING_PTR(rbKey);
-  
-  // Return 1 upon success
-  return cKey;
-  
-}
-
-double unif_rand(unsigned char* seed, int seedLen) {
-
-  unsigned char* prBits;
-  double result;
-  
-  RAND_bytes(prBits, 2*seedLen / 8);
-  
-  memcpy(&result, prBits, sizeof(double));
-  
-  return result;
-  
-}
+static VALUE ope_rb_hgd;
 
 static VALUE ope_rb_rhyper(VALUE self, VALUE rbNn1in, VALUE rbNn2in, VALUE rbKkin, VALUE rbSeed) {
   
@@ -353,123 +298,14 @@ static VALUE ope_rb_rhyper(VALUE self, VALUE rbNn1in, VALUE rbNn2in, VALUE rbKki
   return DBL2NUM(ix);
   
 }
-/*
-static VALUE ope_rb_domain_gap(VALUE self, double ndomain, double nrange, double rgap) {
-  
-  return ope_rb_hyper(rgap, ndomain, nrange-ndomain);
-  
-}
-
-static VALUE ope_rb_lazy_sample(VALUE self, double, d_lo, double d_hi, double r_lo, double r_hi, go_low) {
-  
-  double ndomain = d_hi - d_lo + 1;
-  double nrange = r_hi - r_lo + 1;
-  
-  if (nrange < ndomain) {
-    rb_raise(rb_eRuntimeError, 'Range smaller than domain.');
-  }
-  
-  if (ndomain == 1) {
-    return ope_domain_range(d_lo, r_lo, r_hi);
-  }
-  
-  dgap_cache 
-  
-}
-
-static VALUE ope_rb_domain_range_initialize(VALUE self, double d_arg, double r_lo_arg, double r_hi_arg) {
-  
-  rb_iv_set(self, "@d_arg", DBL2NUM(d_arg));
-  rb_iv_set(self, "@r_lo_arg", DBL2NUM(r_lo_arg));
-  rb_iv_set(self, "@r_hi_arg", DBL2NUM(r_hi_arg));
-  
-  return self;
-  
-}
-*/
-/*
-double sampleHGD(double lowD, double highD, double lowR, double highR, double y, double coins, unsigned int coinsLen) {
-  
-  double whiteBalls, blackBalls, ballsPicked;
-  
-  whiteBalls = highD - lowD + 1;
-  blackBalls = highR - lowR + 1 - whiteBalls;
-  
-  ballsPicked = y - lowR;
-  
-  if ((whiteBalls > 0) && (blackBalls >= 0) && (y >= lowR)) {
-    rb_raise(rb_eRuntimeError, "HGD sampling problem");
-  } 
-  
-  unsigned int precision = (unsigned int) numBits(highR-lowR+1) + 10;
-  
-  return lowD + hgd(ballsPicked, whiteBalls, blackBalls, coins, coinsLen);
-  
-}
-
-double tapeGen(double lowD, double highD, double y, unsigned int desiredNoBits) {
-
-  unsigned int desiredBytes;
-  
-  unsigned int totalLen;
-  totalLen = sizeof(lowD) + sizeof(highD) + sizeof(y);
-  
-  unsigned char lowDBytes[sizeof(lowD)];
-  unsigned char highDBytes[sizeof(highD)];
-  unsigned char yBytes[sizeof(y)];
-  unsigned char concat[totalLen];
-  unsigned char shaDigest[SHA256_DIGEST_LEN];
-  unsigned char seed[AES_BLOCK_BYTES];
-  
-  int i;
-  
-  if (desiredNoBits % 8 == 0) {
-    rb_raise(rb_eRuntimeError, "desiredNoBits is not a multiple of 8");
-  }
-  
-  desiredBytes = desiredNoBits / bitsPerByte;
-  
-  memcpy(&lowD, lowDBytes, sizeof(lowD));
-  memcpy(&highD, highDBytes, sizeof(highD));
-  memcpy(&y, y, sizeof(y));
-  
-  for (i = 0; i < totalLen; i++) {
-    concat[i] = i < sizeof(lowD) ? lowDBytes[i] :
-                (i < sizeof(lowD) + sizeof(highD) ?
-                highDBytes[i] : yBytes[i]);
-  }
-  
-  unsigned char* buffer = malloc(SHA256_DIGEST_LEN);
-  SHA256_CTX sha256;
-  SHA256_Init(&sha256);
-  SHA256_Update(&sha256, concat, totalLen);
-  SHA256_Final(shaDigest, &sha256);
-  
-  AES_encrypt(shaDigest, seed, {0x00});
-  
-  if (AES_BLOCK_BYTES >= desiredBytes) {
-    //...
-  }
-  
-  setSeed(seed);
-  
-  return randomLen(desiredNoBits);
-  
-}*/
 
 void Init_native(void) {
   
 	ope_rb = rb_define_module("OPE");
 	
-	ope_rb_cipher = rb_define_class_under(ope_rb, "Cipher", rb_cObject);
-	
-	rb_define_method(ope_rb_cipher, "initialize", ope_rb_initialize, 1);
-	//rb_define_method(ope_rb_cipher, "domain_gap", ope_rb_domain_gap, 3);
-	rb_define_method(ope_rb_cipher, "rhyper", ope_rb_rhyper, 4);
+	ope_rb_hgd = rb_define_class_under(ope_rb, "HGD", rb_cObject);
+	rb_define_singleton_method(ope_rb_hgd, "rhyper_native", ope_rb_rhyper, 4);
 
-	//ope_rb_domain_range = rb_define_class_under(ope_rb, "DomainRange", rb_cObject);
-	//rb_define_method(ope_rb_domain_range, "initialize", ope_rb_domain_range_initialize, 3);
-	
   return;
 	
 }
